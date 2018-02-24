70d5859a35b5ace75bc6c7c0bb960f2423bdeac4
Sean McGlincy <swmcglincy@hotmail.com>
2018-02-24 14:10:32 -0500

    synonyms 0.675

--- src/main/python/search.py
+++ src/main/python/search.py
@@ -1,13 +1,16 @@
 import re as re
 import math
+# import nltk
 from nltk import PorterStemmer
 from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import wordnet
 
 from nltk.corpus import stopwords
 from readers import read_queries, read_documents
-max = 1
+max_doc = 1
 inverted_index = {}
 doc_length = {}
+word_map = {}
 
 
 def remove_not_indexed_toknes(tokens):
@@ -24,10 +27,7 @@ def tf(freq):
     return 1 + math.log(float(freq))
 
 def idf(freq):
-    return math.log((max - 1) / float(freq))
-
-
-
+    return math.log((max_doc - 1) / float(freq))
 
 
 
@@ -45,8 +45,8 @@ def rank_postings(query):
             query_word_count[query[i]] = 1
             query_word_unique.append(query[i])
 
-    scores = [0] * max  # Make max of list
-    length = [0] * max  # Make max of list
+    scores = [0] * max_doc  # Make max of list
+    length = [0] * max_doc  # Make max of list
     query_length = 0
 
     for i in range(len( query_word_unique)):
@@ -94,10 +94,10 @@ def rank_postings(query):
 
 
 def search_query(query):
-    tokens = tokenize(str(query['query']))
-    # tokens = tokenize_search(str(query['query']))
+    # tokens = tokenize(str(query['query']))
+    tokens = tokenize_search(str(query['query']))
     indexed_tokens = remove_not_indexed_toknes(tokens)
-    if query['query number'] == 44:
+    if query['query number'] == 22:
         print(">>>>>>>>>>>>>  Query: ", indexed_tokens)
     if len(indexed_tokens) == 0:
         return []
@@ -117,11 +117,11 @@ def remove_hyphen(tokens, char):
 #     return str
 def stemming(tokens):
     # https://stackoverflow.com/questions/10369393/need-a-python-module-for-stemming-of-text-documents
-    stop_words = set(stopwords.words('english'))  #https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
+    # stop_words = set(stopwords.words('english'))  #https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
     str = []
     for token in tokens:
-        if token not in stop_words:
-            str.append(PorterStemmer().stem(token))
+        # if token not in stop_words:
+        str.append(PorterStemmer().stem(token))
 
     return str
 
@@ -156,15 +156,20 @@ def specialChar(tokens):
         # Remove numbers that are in a string of word
         number = re.sub('[^\d]', '', word)
 
+        char = []
         # Subtract the two sets and remove the digits
-        char = word.replace(number, "")
+        # char = word.replace(number, "")
+        if len(number) > 0:
+            char = word.split(number)
 
 
         # Add number and char if they aren't duplicates
         if  len(number) > 0 and number != word:
             str.append(number)
         if  len(char) > 0 and char != word:
-            str.append(char)
+            for c in char:
+                if len(c) > 0:
+                    str.append(c)
 
 
         # if len(word) > 0:
@@ -181,7 +186,9 @@ def wordPairs(tokens):
         if len(first) > 0 and first != ".":
             if len(second) > 0 and  second != ".":
                 val = "%s %s" %(first,second)
+                # val2 = "%s %s" %(second, first)
                 str.append(val)
+                # str.append(val2)
     tokens.extend(str)
     return tokens
 
@@ -195,46 +202,172 @@ def stopWords(tokens):
 
     return str
 
-    # stop_words_list = ["a", "an", "the", "be", "been", "you", "are", "you're", "by", "to", "ing"]
+
+def creat_synonyms_list():
+    word_list = [
+        ("investigations" ,  "effects"),
+        ("liquid", "water"),
+        ("wave resistance" , "wave system"),
+        ("velocities" , "pressure"),
+        ("distribution" , "transportation"),
+        ("speed", "velocity"),
+        ("fast", "hypersonic"),
+        ("velocious", "hypersonic"),
+        ("variation", "differences"),
+        ("variation", "different"),
+        ("discover", "test"),
+        ("discover", "observe"),
+        ("discover", "recognize"),
+        ("discover", "realize"),
+        ("gas", "air"),
+        ("demonstrate", "discover"),
+        ("discover", "application"),
+        ("turbulent", "violent"),
+        ("turbulent", "unstable"),
+        ("degrees", "temperature"),
+        ("super sonic", "high speed"),
+        ("information", "paper"),
+        ("information", "analysis"),
+        ("centrifugal", "various"),
+        ("circle", "various"),
+        ("small", "various"),
+        ("newtonian", "normal"),
+        ("boat-tail", "projectile"),
+        ("boat-tail", "weapon"),
+        ("boat-tail", "missile"),
+        ("paper", "example"),
+        ("gases", "chemical"),
+        ("gases", "molecules"),
+        ("acoustic", "sound"),
+        ("acoustic", "subsonic"),
+        ("acoustic", "sonic"),
+        # ("acoustic", "shock"),
+        ("propagation", "linearized"),
+        ("propagation", "absorption"),
+        ("papers", "experimental"),
+        ("papers", "prove"),
+        ("prove", "results"),
+        ("reacting gases", "dissociating gas"),
+        ("reacting", "conducting"),
+        ("acoustic", "noise"),
+        ("paper", "predictions"),
+        ("paper", "theory"),
+        ("around", "yawed"),
+        ("flow", "supersonic"),
+        ("flow", "speed"),
+        ("distance", "range"),
+        ("linear function", "coefficients"),
+        ("reynolds numbers", "flow"),
+        # ("flow", "pressure"),
+        # ("conditions", "coefficients"),
+        # ("wave", "radiation"),
+        # ("acoustic", "aerodynamic"),
+        # ("prove", "ideal"),
+        # ("acoustic", "harmonic"),
+        # ("acoustic", "vibration"),
+        # ("wave", "frequency"),
+        #
+        # ("various", "semi"),
+        # ("hypersonic", "force"),
+        # ("incidence", "degree"),
+        # ("incidence", "occurrence"),
+
+
+
+        # ("supersonic", "high speed"),
+        # ("thermal", "heat"),
+        # ("friction", "resistance"),
+        # # ("kinetic", "particle")
+
+        # ("friction", "motion"),
+        # ("temperature", "pressure"),
+        # ("approximations", "similarity"),
+        # ("air", "temperature"),
+        # ("kinetic", "energy"),
+        # ("theoretical", "theory"),
+        # ("temperature", "thermal"),
+        # ("temperature", "heat")
+
+    ]
+
+    # word_map = {}
+    for tup in word_list:
+        w1 = PorterStemmer().stem(tup[0])
+        w2 = PorterStemmer().stem(tup[1])
+        # Forward mapping
+        word_map[w1] = w2
+        #  Reverse mapping
+        word_map[w2] = w1
+
+
+
+def synonyms(tokens):
+    syn = []
+    for token in tokens:
+        if token in word_map:
+            syn.append(word_map[token])
+            # print (word_list[token])
+
+    tokens.extend(syn)
+    return tokens
+
+    # syn = []
+    # word_list = {
+    #     "investigations" : "effects",
+    #      "effects" : "investigations",
+    #     "investigation": "effect",
+    #     "effect": "investigation",
+    #     "liquid": "water",
+    #     "water" : "liquid",
+    #     "wave resistance" : "wave system",
+    #     "velocities" : "pressure",
+    #     "pressure" : "velocities",
+    #     "distribution" : "transportation",
+    #      "transportation" : "distribution"
     #
+    # }
     #
+    # for token in tokens:
+    #     if token in word_list:
+    #         syn.append(word_list[token])
+    #         # print (word_list[token])
     #
-    # for word in stop_words_list:
-    #     if word in tokens:
-    #         tokens.remove(word)
+    # if len(syn) > 1:
+    #     tokens.extend(syn)
+    # return tokens
+
+    #  From https://www.geeksforgeeks.org/get-synonymsantonyms-nltk-wordnet-python/
+    # from nltk.corpus import wordnet
+    # nltk.corpus.wordnet
+
+    # synonyms = []
+    # for token in tokens:
+    #     for syn in wordnet.synsets(token):
+    #         for l in syn.lemmas():
+    #             synonyms.append(l.name())
+    #
+    # tokens.extend(list(set(synonyms)))
+    # # for v in val:
+    # #     tokens.append(v)
     # return tokens
 
 
+def tokenize_search(text):
+    tokens = []
 
-# def stemming_snowball(tokens):
-#     stemmer = SnowballStemmer("english",  ignore_stopwords=True)    # http://www.nltk.org/howto/stem.html
-#     stop_words = set(stopwords.words('english'))  #https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
+
+    tokens = text.split(" ")
+    tokens = remove_hyphen(tokens, "-")
+    tokens = remove_hyphen(tokens, ",")
+    tokens = remove_hyphen(tokens, "=")
+    tokens = stopWords(tokens)
+    # tokens = synonyms(tokens)
+    tokens = specialChar(tokens)
+    tokens = wordPairs(tokens)
+    tokens = mapNumbers(tokens)
+    tokens = stemming(tokens)
 #
-#     str = []
-#     new_list = []
-#     for token in tokens:
-#         if token not in stop_words:
-#             str.append(stemmer.stem(token))
-#
-#     if(len(str) > 0):
-#         new_list.extend(str)
-
-
-
-
-# def tokenize_search(text):
-#     tokens = []
-#     tokens = text.split(" ")
-#     # tokens = stopWords(tokens)
-#     tokens = remove_hyphen(tokens, "-")
-#     tokens = remove_hyphen(tokens, ",")
-#     tokens = remove_hyphen(tokens, "=")
-#
-#     tokens = mapNumbers(tokens)
-#     tokens = stemming_snowball(tokens)
-#     tokens = specialChar(tokens)
-#
-#     return tokens
+    return tokens
 
 def tokenize(text):
     tokens = []
@@ -243,22 +376,12 @@ def tokenize(text):
     tokens = remove_hyphen(tokens, ",")
     tokens = remove_hyphen(tokens, "=")
     tokens = stopWords(tokens)
+    # tokens = synonyms(tokens)
     tokens = specialChar(tokens)
-
     tokens = wordPairs(tokens)
-
-
-
     tokens = mapNumbers(tokens)
     tokens = stemming(tokens)
-    # tokens = stemming_snowball(tokens)
-    # tokens = specialChar(tokens)
-    # tokens = wordPairs(tokens)  # decreased score
-
-
-
-
-
+    tokens = synonyms(tokens)
 
 
     return tokens
@@ -346,13 +469,13 @@ def add_to_index(document):
     # tokens.extend(author)
 
     # Metadata
-    global max
-    max += 1
-    # doc_length[int(doc_id)] = len(tokens)
+    global max_doc
+    max_doc += 1
 
 
     for token in tokens:
         add_token_to_index(token, doc_id)
+
 
 
 def create_index():
@@ -362,6 +485,7 @@ def create_index():
     print ("Created index with size {}".format(len(inverted_index)))
     # print_inverted_index()
 
+creat_synonyms_list()
 create_index()
 
 if __name__ == '__main__':
