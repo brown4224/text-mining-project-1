93d09c9ba0cbc2407af1c8970528e77162d6d9f2
Sean McGlincy <swmcglincy@hotmail.com>
2018-03-02 20:41:28 -0500

    clean up and Lang model

--- src/main/python/search.py
+++ src/main/python/search.py
@@ -1,3 +1,9 @@
+#  Sean McGlincy
+#  Text Mining: Project 1
+#  March 2, 2018
+#  Score: 0.6846613085682793
+
+
 import re as re
 import math
 # import nltk
@@ -19,10 +25,6 @@ def remove_not_indexed_toknes(tokens):
 def remove_duplicates(tokens):
     return list(set(tokens))
 
-
-
-
-
 def tf(freq):
     return 1 + math.log(float(freq))
 
@@ -30,8 +32,57 @@ def idf(freq):
     return math.log((max_doc - 1) / float(freq))
 
 
+def lang_model(query):
+    # 0.1  = Final ncdg for all queries is 0.46135264398948
+    # 0.25 = Final ncdg for all queries is 0.4738344928695038
+    # 0.5  = Final ncdg for all queries is 0.5017062653731896
+    # 0.75 = Final ncdg for all queries is 0.520379967369712
+    # 0.9  = Final ncdg for all queries is 0.5218121221541857
 
-def rank_postings(query):
+
+    query_word_unique = list(set(query))
+    scores = [1] * max_doc  # Make max of list
+    scores[0] = 0
+
+
+    # Start Algorithm
+    for token in query_word_unique:
+        id_list = inverted_index[token]
+
+        # collection Freqency
+        c_tf = 0
+        c_len = 0
+        mc = 0
+        for tup in id_list:
+            c_tf += tup[1]
+            c_len += doc_length[tup[0]]
+        if c_len > 0:
+            mc = c_tf / c_len
+
+        # Doc Frequency
+        for i in range(1, len(scores)):
+            doc_id = i
+            d_tf = 0
+            d_len = 0
+            md = 0
+            for tup in id_list:
+                if i == tup[0]:
+                    d_tf = tup[1]
+            if doc_length[doc_id] > 0:
+                md = d_tf / doc_length[doc_id]
+
+            # Lambda
+            l = 0.75
+            scores[i] *= (l * md) + ((1.0 - l) * mc)
+
+    ranking = []
+    for i in range(0, len(scores)):
+        ranking.append((i, scores[i]))
+    ranking.sort(key=lambda tup: tup[1], reverse=True)
+    # print(ranking)
+    return [pos[0] for pos in ranking]
+
+def cos_ranking(query):   #0.6117392006777134
     query_word_count = {}
     query_word_count[query[0]] = 1
     query_word_unique = [query[0]]
@@ -48,6 +99,7 @@ def rank_postings(query):
     scores = [0] * max_doc  # Make max of list
     length = [0] * max_doc  # Make max of list
     query_length = 0
+
 
     for i in range(len( query_word_unique)):
         # Variables
@@ -71,6 +123,8 @@ def rank_postings(query):
             length[doc_id] += vec_doc**2  #  length
             scores[doc_id] += vec_doc * vec_query  #  Cos score
 
+
+
     ranking = []
     for i in range(0, len(scores)):
 
@@ -83,28 +137,22 @@ def rank_postings(query):
 
         ranking.append((i, cos_score ))
     ranking.sort(key=lambda tup: tup[1], reverse=True)
-    # print(ranking)
 
 
 
-    # return [pos[0] for pos in ranking if pos[1] > 0]
-    return [pos[0] for pos in ranking]  #  Get a higher score for returning more!!!
-
-
-
+    return [pos[0] for pos in ranking]
 
 def search_query(query):
     # tokens = tokenize(str(query['query']))
     tokens = tokenize_search(str(query['query']))
     indexed_tokens = remove_not_indexed_toknes(tokens)
-    # if query['query number'] == 22:
-        # print(">>>>>>>>>>>>>  Query: ", indexed_tokens)
     if len(indexed_tokens) == 0:
         return []
     elif len(indexed_tokens) == 1:
         return inverted_index[indexed_tokens[0]]
     else:
-        return rank_postings(indexed_tokens)
+        # return lang_model(indexed_tokens)
+        return cos_ranking(indexed_tokens)
 
 def remove_hyphen(tokens, char):
     str = []
@@ -113,11 +161,8 @@ def remove_hyphen(tokens, char):
     return str
 
 
-
-#     return str
 def stemming(tokens):
     # https://stackoverflow.com/questions/10369393/need-a-python-module-for-stemming-of-text-documents
-    # stop_words = set(stopwords.words('english'))  #https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
     str = []
     for token in tokens:
         # if token not in stop_words:
@@ -126,11 +171,8 @@ def stemming(tokens):
     return str
 
 
-
 def stemming_snowball(tokens):
     stemmer = SnowballStemmer("english",  ignore_stopwords=True)    # http://www.nltk.org/howto/stem.html
-    stop_words = set(stopwords.words('english'))  #https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
-
     str = []
     new_list = []
     for token in tokens:
@@ -172,7 +214,6 @@ def specialChar(tokens):
                     str.append(c)
 
 
-        # if len(word) > 0:
         str.append(word)
 
     return str
@@ -186,9 +227,7 @@ def wordPairs(tokens):
         if len(first) > 0 and first != ".":
             if len(second) > 0 and  second != ".":
                 val = "%s %s" %(first,second)
-                # val2 = "%s %s" %(second, first)
                 str.append(val)
-                # str.append(val2)
     tokens.extend(str)
     return tokens
 
@@ -199,14 +238,13 @@ def stopWords(tokens):
     for token in tokens:
         if token not in stop_words:
             str.append(token)
-
     return str
 
 
 def creat_synonyms_list():
     word_list = [
 
-        # doc -> query word
+        # (doc -> query word) mapping
         ("investigations", "effects"),
         ("effects", "investigations"),
         ("liquid", "water"),
@@ -335,82 +373,14 @@ def creat_synonyms_list():
         ("estimates", "model"),
         ("analogue computer", "model"),
         ("affecting", "influence"),
-        # ("investigation", "influence"),
         ("roughness", "turbulent"),
-        # ("formulae", "model"),
-        # ("analytical", "model"),
-        # ("supersonic", "laminar"),
-        # ("measurements", "parameters"),
-        # ("axis", "linear"),
-
-        # ("example", "find"),
-        # ("infite", "data"),
-        # ("temperature", "pressure"),
-        # ("coefficients", "data"),
-        # ("heat", "pressure"),
-
-
-
-
-        # ("frequency", "wave"),
-        # ("turbulent", "wind"),
-        # ("pressure", "laminar"),
-        #
-        #
-        #
-        #
-        # ("mach", "supersonic"),
-        #
-        # ("conditions", "coefficients"),
-        # ("wave", "radiation"),
-        # ("acoustic", "aerodynamic"),
-        # ("prove", "ideal"),
-        # ("acoustic", "harmonic"),
-        # ("acoustic", "vibration"),
-        # ("wave", "frequency"),
-        #
-        # ("various", "semi"),
-        # ("hypersonic", "force"),
-        # ("incidence", "degree"),
-        # ("incidence", "occurrence"),
-        #
-        #
-        #
-        # ("supersonic", "high speed"),
-        # ("thermal", "heat"),
-        # ("friction", "resistance"),
-        # # ("kinetic", "particle")
-        #
-        # ("friction", "motion"),
-        # ("temperature", "pressure"),
-        # ("approximations", "similarity"),
-        # ("air", "temperature"),
-        # ("kinetic", "energy"),
-        # ("theoretical", "theory"),
-        # ("temperature", "thermal"),
-        # ("temperature", "heat")
 
     ]
-    # for tup in word_list:
-    #     w1 = PorterStemmer().stem(tup[0])
-    #     w2 = PorterStemmer().stem(tup[1])
-    #     # Forward mapping
-    #     if w1 in word_map:
-    #         word_map[w1].append(w2)
-    #     else:
-    #         word_map[w1] = [w2]
-    #     #  Reverse mapping
-    #     if w2 in word_map:
-    #         word_map[w2].append(w1)
-    #     else:
-    #         word_map[w2] = [w1]
 
 
     for tup in word_list:
-
         w1 = PorterStemmer().stem(tup[0])
         w2 = PorterStemmer().stem(tup[1])
-
         if w1 in word_map:
             if w2 not in word_map[w1]:
                 word_map[w1].append(w2)
@@ -418,70 +388,44 @@ def creat_synonyms_list():
             word_map[w1] = [w2]
 
 
-        # if w2 in word_map:
-        #     if w1 not in word_map[w2]:
-        #         word_map[w2].append(w1)
-        # else:
-        #     word_map[w2] = [w1]
-        # Forward mapping
-        # word_map[w1] = w2
-         # Reverse mapping
-        # word_map[w2] = w1
-
-    # for key, value in word_map.items():
-    #     print("(\"",key, "\" , \"", value, "\")," )
-
-
-
 def synonyms(tokens):
     syn = []
     for token in tokens:
         if token in word_map:
             syn.extend(word_map[token])
-            # print (word_map[token])
-
-
     tokens.extend(syn)
     return tokens
 
 
-
-
 def tokenize_search(text):
     tokens = []
-
-
     tokens = text.split(" ")
     tokens = remove_hyphen(tokens, "-")
     tokens = remove_hyphen(tokens, ",")
-    tokens = remove_hyphen(tokens, "=")
+    # tokens = remove_hyphen(tokens, "=")
     tokens = remove_hyphen(tokens, "\/")
     tokens = stopWords(tokens)
-    # tokens = synonyms(tokens)
     tokens = specialChar(tokens)
     tokens = wordPairs(tokens)
     tokens = mapNumbers(tokens)
     tokens = stemming(tokens)
-    # tokens = synonyms(tokens)
     return tokens
+
 
 def tokenize(text):
     tokens = []
-    tokens = text.split(" ")
-    tokens = remove_hyphen(tokens, "-")
-    tokens = remove_hyphen(tokens, ",")
-    tokens = remove_hyphen(tokens, "=")
-    tokens = stopWords(tokens)
-    tokens = specialChar(tokens)
-    tokens = wordPairs(tokens)
-    tokens = mapNumbers(tokens)
-    tokens = stemming(tokens)
-    tokens = synonyms(tokens)
-
-
+    tokens = text.split(" ")                   # Cos Base:  0.6117392006777134
+    tokens = remove_hyphen(tokens, "-")        # Hyphen  :  0.6347618360585855
+    tokens = remove_hyphen(tokens, ",")        # Comma   :  0.6407339532992602
+    # tokens = remove_hyphen(tokens, "=")      # Equal   :  0.6407174038997508
+    tokens = remove_hyphen(tokens, "\/")       # Slash   :  0.6407339532992602
+    tokens = stopWords(tokens)                 # Stop    :  0.6372894149977122
+    tokens = specialChar(tokens)               # Char    :  0.6485518460375124
+    tokens = wordPairs(tokens)                 # Pairs   :  0.6407921165516753
+    tokens = mapNumbers(tokens)                # Num     :  0.6411075910761688
+    tokens = stemming(tokens)                  # Stem    :  0.6683700827175519
+    tokens = synonyms(tokens)                  # Synon   :  0.6846613085682793
     return tokens
-
-
 
 
 def mapNumbers(tokens):
@@ -531,6 +475,7 @@ def mapNumbers(tokens):
     tokens.extend(str)
     return tokens
 
+
 def print_inverted_index():
     for key, value in inverted_index.items():
         print(key)
@@ -552,37 +497,32 @@ def add_token_to_index(token, doc_id):
     else:
         inverted_index[token] = [[doc_id, 1]]
 
-# https://www.geeksforgeeks.org/python-get-unique-values-list/
 def add_to_index(document):
+    # https://www.geeksforgeeks.org/python-get-unique-values-list/
     doc_id = document['id']
     tokens = []
     tokens = tokenize(document['title'])
     body = tokenize(document['body'])
-    # author = tokenize(document['author'])
-
     tokens.extend(body)
-    # tokens.extend(author)
+
 
     # Metadata
     global max_doc
     max_doc += 1
+    doc_length[document['id']] = len(document['title']) + len(document['body'])
 
 
     for token in tokens:
         add_token_to_index(token, doc_id)
-
-
 
 def create_index():
     for document in read_documents():
 
         add_to_index(document)
     print ("Created index with size {}".format(len(inverted_index)))
-    # print_inverted_index()
 
 creat_synonyms_list()
 create_index()
-
 
 if __name__ == '__main__':
     all_queries = [query for query in read_queries() if query['query number'] != 0]
